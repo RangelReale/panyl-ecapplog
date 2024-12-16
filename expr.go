aedee34afd151e65182d3584590eee299fca0f61
Rangel Reale <rangelspam@gmail.com>
2024-12-16 10:26:40 -0300

    expr constants

---
+++ expr.go
@@ -0,0 +1,9 @@
+package panylecapplog
+
+func ExprConstants() map[string]any {
+	return map[string]any{
+		"MetadataECAppLogExtraCategories": MetadataExtraCategories,
+		"MetadataECAppLogColor":           MetadataColor,
+		"MetadataECAppLogBGColor":         MetadataBGColor,
+	}
+}
---
+++ metadata.go
@@ -0,0 +1,7 @@
+package panylecapplog
+
+const (
+	MetadataExtraCategories = "_ecapplog_extra_categories"
+	MetadataColor           = "_ecapplog_color"
+	MetadataBGColor         = "_ecapplog_bgcolor"
+)
